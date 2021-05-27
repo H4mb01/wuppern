b917090ef424ef88c8743c13cf2462bdc6ca33ab
H4mb01 <manuel@muenstedt.de>
2021-05-27 06:47:23 +0200

    erster upload

---
+++ src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <Karte Wert="7" Farbe="Karo" />
+  </div>
+</template>
+
+<script>
+  import Karte from "@/components/Karte.vue"
+  export default {
+    name: "About",
+    components: {
+      Karte,
+    }
+  }
+</script>
+
+<style scoped>
+  .about {
+    position: relative;
+    height: 200px;
+    width: 100px;
+    margin: auto;
+  }
+</style>
